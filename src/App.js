b575978e01bada25a758ff59436dd95917eacb12
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-04-17 09:34:19 -0400

    removed prop drilling and replaced with useContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 // Create Random Posts Function
@@ -12,6 +12,7 @@ function createRandomPost() {
 // Steps for context
 // 1. create new context
 // 2. provide value to child components
+// 3. consume context value
 
 // Context
 const PostContext = createContext()
@@ -66,39 +67,39 @@ function App() {
           {isFakeDark ? "☀️" : "🌙"}
         </button>
 
-        <Header
-          posts={searchedPosts}
-          onClearPosts={handleClearPosts}
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
-          />
-        <Main posts={searchedPosts} onAddPost={handleAddPost} />
-        <Archive onAddPost={handleAddPost} />
+        <Header />
+        <Main />
+        <Archive />
         <Footer />
       </section>
     </PostContext.Provider>
   );
 }
 
-function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
+// Header Component
+function Header() {
+  // read value from context
+  const {onClearPosts} = useContext(PostContext)
+
   return (
     <header>
       <h1>
         <span>⚛️</span>The Atomic Blog
       </h1>
       <div>
-        <Results posts={posts} />
-        <SearchPosts
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
-        />
+        <Results />
+        <SearchPosts />
         <button onClick={onClearPosts}>Clear posts</button>
       </div>
     </header>
   );
 }
 
-function SearchPosts({ searchQuery, setSearchQuery }) {
+// Search Posts Component
+function SearchPosts() {
+  // read values from context
+  const {searchQuery, setSearchQuery} = useContext(PostContext)
+
   return (
     <input
       value={searchQuery}
@@ -108,28 +109,38 @@ function SearchPosts({ searchQuery, setSearchQuery }) {
   );
 }
 
-function Results({ posts }) {
+// Results Component
+function Results() {
+  // read value from context
+  const {posts} = useContext(PostContext)
+
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-function Main({ posts, onAddPost }) {
+// Main Component
+function Main() {
   return (
     <main>
-      <FormAddPost onAddPost={onAddPost} />
-      <Posts posts={posts} />
+      <FormAddPost />
+      <Posts />
     </main>
   );
 }
 
-function Posts({ posts }) {
+// Posts Component
+function Posts() {
   return (
     <section>
-      <List posts={posts} />
+      <List />
     </section>
   );
 }
 
-function FormAddPost({ onAddPost }) {
+// Form Add Post Component
+function FormAddPost() {
+  // read value from context
+  const {onAddPost} = useContext(PostContext)
+
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
@@ -158,7 +169,11 @@ function FormAddPost({ onAddPost }) {
   );
 }
 
-function List({ posts }) {
+// List Component
+function List() {
+  // read value from context
+  const {posts} = useContext(PostContext)
+
   return (
     <ul>
       {posts.map((post, i) => (
@@ -171,7 +186,11 @@ function List({ posts }) {
   );
 }
 
-function Archive({ onAddPost }) {
+// Archive Component
+function Archive() {
+  // read value from context
+  const {onAddPost} = useContext(PostContext)
+
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
@@ -203,6 +222,7 @@ function Archive({ onAddPost }) {
   );
 }
 
+// Footer Component
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
 }
